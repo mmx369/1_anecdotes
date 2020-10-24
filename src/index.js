67723e54020def489ee2cb6f9cae7974d02b0c47
mmx369 <maksim@klishevich.com>
2020-10-24 22:43:13 +0300

    new comm

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [voted, setVoted] = useState(new Array(6).fill(0))
+
+  console.log(1111, voted);
 
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -14,12 +17,31 @@ const App = (props) => {
     setSelected(getRandomIntInclusive(0, 5))
   }
 
+  const voteAnecdote = () => {
+    const copy = [...voted]
+    copy[selected] += 1
+    setVoted(copy)
+  }
+  const selectEnd = () => {
+    if (voted[selected] === 1) {
+      return 'vote'
+    }
+    return 'votes'
+  }
+
+  const i = voted.indexOf(Math.max(...voted));
+
   return (
     <div>
-      {props.anecdotes[selected]}
+      <h3>Anecdote of the day</h3>
+      {props.anecdotes[selected]} <br />
+      has {voted[selected]} {selectEnd()}
       <p>
         <button onClick={nextAnecdote}>next anecdote</button>
+        <button onClick={voteAnecdote}>vote</button>
       </p>
+      <h3>Anecdotes with most votes</h3>
+      {props.anecdotes[i]}
     </div>
   )
 }
